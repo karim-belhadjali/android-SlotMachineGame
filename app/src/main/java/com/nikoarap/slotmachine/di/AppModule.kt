22eacbe5a4b6fc54a28ff3e92bf4a0a825e47b0c
kbelhadjali <kbelhadjali@vermeg.com>
2021-06-17 00:55:54 +0200

    deleted useless import from app module

--- app/src/main/java/com/nikoarap/slotmachine/di/AppModule.kt
+++ app/src/main/java/com/nikoarap/slotmachine/di/AppModule.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.room.Room
 import com.nikoarap.slotmachine.db.classes.JackpotDataBase
-jackimport com.nikoarap.slotmachine.db.dao.JackpotDao
+import com.nikoarap.slotmachine.db.dao.JackpotDao
 import com.nikoarap.slotmachine.other.Constants.JACKPOT_DATABASE_NAME
 import com.nikoarap.slotmachine.other.Constants.KEY_NAME
 import com.nikoarap.slotmachine.other.Constants.KEY_TOGGLE_FIRST_TIME
