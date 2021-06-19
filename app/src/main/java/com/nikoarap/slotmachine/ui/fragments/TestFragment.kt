c863656c438777808b9fa1880dff10e9ec52f748
kbelhadjali <kbelhadjali@vermeg.com>
2021-06-19 16:18:42 +0200

    fragment_test_dahsboard.xml.xml Register design inmplemented succesfully

--- app/src/main/java/com/nikoarap/slotmachine/ui/fragments/TestFragment.kt
+++ app/src/main/java/com/nikoarap/slotmachine/ui/fragments/TestFragment.kt
@@ -1,12 +1,8 @@
 package com.nikoarap.slotmachine.ui.fragments
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.nikoarap.slotmachine.R
 
-class TestFragment : Fragment(R.layout.fragment_test) {
+class TestFragment : Fragment(R.layout.fragment_test_register) {
 
 }
