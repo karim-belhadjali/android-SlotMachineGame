174422bca6f8734c7d5a106d87890d2e116deb99
kbelhadjali <kbelhadjali@vermeg.com>
2021-06-19 16:47:38 +0200

    fragment_test_thank_you.xml.xml.xml Tnak you design inmplemented succesfully

--- app/src/main/java/com/nikoarap/slotmachine/ui/fragments/TestFragment.kt
+++ app/src/main/java/com/nikoarap/slotmachine/ui/fragments/TestFragment.kt
@@ -3,6 +3,6 @@ package com.nikoarap.slotmachine.ui.fragments
 import androidx.fragment.app.Fragment
 import com.nikoarap.slotmachine.R
 
-class TestFragment : Fragment(R.layout.fragment_test_register) {
+class TestFragment : Fragment(R.layout.fragment_test_thank_you) {
 
 }
